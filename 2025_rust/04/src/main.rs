171f44abe4f537dd760592f71d860a2372da62b5
frostnight0 <westforest0@gmail.com>
2025-12-11 18:36:45 +0200

    up

--- 2025_rust/04/src/main.rs
+++ 2025_rust/04/src/main.rs
@@ -1,153 +1,171 @@
-fn get_vec(file: &str) -> Vec<Vec<char>> {
-    let col = file.lines().next().unwrap().len();
-    let mut row = 0;
+// fn type_of<T>(_: T) -> &'static str {
+//     std::any::type_name::<T>()
+// }
+//
+// fn print_matrix(matrix: &Vec<Vec<char>>) {
+//     for (idx_word, word) in matrix.iter().enumerate() {
+//         for (idx_letter, letter) in word.iter().enumerate() {
+//             print!("{}", letter);
+//         }
+//         println!();
+//     }
+// }
+
+fn get_matrix(file: &str) -> Vec<Vec<char>> {
+    // print!("{}", file);
+    let word_size = file.lines().next().unwrap().len();
+    let mut word_count = 0;
     for _ in file.lines() {
-        row += 1;
+        word_count += 1;
     }
 
-    //println!("{} x {}", row, col);
+    // println!("word_count: {word_count} x word_size: {word_size}");
 
-    let mut vec = vec![vec![' '; col]; row];
+    let mut matrix = vec![vec![' '; word_size]; word_count];
 
-    let mut a: usize = 0;
-    let mut b: usize = 0;
-
-    for x in file.lines() {
-        // println!("{}", y);
-        for y in x.chars() {
-            // println!("{}", x);
-            vec[a][b] = y;
-            b += 1;
+    for (idx_word, word) in file.lines().enumerate() {
+        // println!("word: {}", word);
+        for (idx_letter, letter) in word.chars().enumerate() {
+            // println!("letter: {}", letter);
+            matrix[idx_word][idx_letter] = letter;
         }
-        a += 1;
-        b = 0;
     }
 
-    vec
+    // println!("----------------------------------------------------------");
+    // print_matrix(&matrix);
+    // println!("----------------------------------------------------------");
+
+    matrix
 }
 
-fn get_neighbours_count(vec: &Vec<Vec<char>>, x: usize, y: usize) -> i32 {
-    //let x = 2;
-    //let y = 0;
-    //println!("[xy][{}{}]", x, y);
-    let x_limit = vec[0].len() -1;
-    let y_limit = vec.len() -1;
+fn check_char(matrix: &Vec<Vec<char>>, w: usize, l: usize) -> i32 {
+    if matrix[w][l] == '@' {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+fn get_neighbours_count(matrix: &Vec<Vec<char>>, w: usize, l: usize) -> i32 {
+    // println!("w:[w], L:[{l}]");
+
+    let w_limit = matrix.len() -1;
+    let l_limit = matrix[0].len() -1;
 
     let mut count = 0;
-    let mut a: usize;
-    let mut b: usize;
-
-    let c = vec[y][x];
-    if c == '.' {
-        return 9;
-    }
-
-    fn check_char(vec: &Vec<Vec<char>>, a: usize, b: usize) -> i32 {
-        //print!("ch: {} {} ", a, b);
-        //println!("[{}]", vec[b][a]);
-        if vec[b][a] == '@' {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+    let mut w_adj: usize;
+    let mut l_adj: usize;
 
     // 1
-    if x > 0 && y > 0 {
-        a = x - 1;
-        b = y - 1;
-        count += check_char(vec, a, b);
+    if w > 0 && l > 0 {
+        w_adj = w - 1;
+        l_adj = l - 1;
+        count += check_char(matrix, w_adj, l_adj);
     }
 
     // 2
-    if y > 0 {
-        a = x;
-        b = y - 1;
-        count += check_char(vec, a, b);
+    if w > 0 {
+        w_adj = w - 1;
+        count += check_char(matrix, w_adj, l);
     }
 
     // 3
-    if x < x_limit && y > 0 {
-        a = x + 1;
-        b = y - 1;
-        count += check_char(vec, a, b);
+    if w > 0 && l < l_limit {
+        w_adj = w - 1;
+        l_adj = l + 1;
+        count += check_char(matrix, w_adj, l_adj);
     }
 
     // 4
-    if x > 0 {
-        a = x - 1;
-        b = y;
-        count += check_char(vec, a, b);
+    if l > 0 {
+        l_adj = l - 1;
+        count += check_char(matrix, w, l_adj);
     }
 
     // 6
-    if x < x_limit {
-        a = x + 1;
-        b = y;
-        count += check_char(vec, a, b);
+    if l < l_limit {
+        l_adj = l + 1;
+        count += check_char(matrix, w, l_adj);
     }
 
     // 7
-    if x > 0 && y < y_limit {
-        a = x - 1;
-        b = y + 1;
-        count += check_char(vec, a, b);
+    if w < w_limit && l > 0 {
+        w_adj = w + 1;
+        l_adj = l - 1;
+        count += check_char(matrix, w_adj, l_adj);
     }
 
     // 8
-    if y < y_limit {
-        a = x;
-        b = y + 1;
-        count += check_char(vec, a, b);
+    if w < w_limit {
+        w_adj = w + 1;
+        count += check_char(matrix, w_adj, l);
     }
 
     // 9
-    if x < x_limit && y < y_limit {
-        a = x + 1;
-        b = y + 1;
-        count += check_char(vec, a, b);
+    if w < w_limit && l < l_limit {
+        w_adj = w + 1;
+        l_adj = l + 1;
+        count += check_char(matrix, w_adj, l_adj);
     }
 
-    //print!("[{}]", count);
+    // print!("[{}]", count);
 
     count
 }
 
-fn get_sum(vec: &Vec<Vec<char>>) -> i32 {
-    for n in vec.iter() {
-        for m in n.iter() {
-            print!("{}", m);
-        }
-            println!("");
-    }
-
-    println!("{} x {}", vec.len(), vec[0].len());
-    let mut nec = vec![vec!['.'; vec.len()]; vec[0].len()];
-
+fn get_sum(matrix: &Vec<Vec<char>>) -> i32 {
     let mut sum = 0;
-    for a in 0..vec[0].len() {
-        for b in 0..vec.len() {
-            //println!("in gb: {} {}", a, b);
-            let count = get_neighbours_count(&vec, a, b);
-            nec[b][a] = char::from_digit(count as u32, 10).unwrap();
-            if count < 4  {
-                println!("sum+ {} {}", sum, count); 
+
+    for (idx_word, _) in matrix.iter().enumerate() {
+        for (idx_letter, letter) in matrix[idx_word].iter().enumerate() {
+            let count = get_neighbours_count(matrix, idx_word, idx_letter);
+            if count < 4 && *letter == '@'  {
                 sum += 1;
-                //nec[a][b] = char::from_digit(count as u32, 10).unwrap();
-            } else {
-                //nec[a][b] = '0';
             }
         }
     }
 
-    for n in nec.iter() {
-        for m in n.iter() {
-            print!("{}", m);
-        }
-            println!("");
-    }
+    // println!("{sum}");
 
     sum
+}
+
+fn delete_rolls(matrix: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut matrix_clone = matrix.clone();
+
+    for (idx_word, _) in matrix.iter().enumerate() {
+        for (idx_letter, letter) in matrix[idx_word].iter().enumerate() {
+            let count = get_neighbours_count(matrix, idx_word, idx_letter);
+            if count < 4 && *letter == '@'  {
+                matrix_clone[idx_word][idx_letter] = 'x';
+            }
+        }
+    }
+
+    // print_matrix(&matrix_clone);
+
+    matrix_clone
+}
+
+fn get_sum_of_rolls(matrix: &Vec<Vec<char>>) -> i32 {
+    let mut matrix = matrix.clone();
+    // print_matrix(&matrix);
+    // println!("{}", type_of(matrix));
+
+    let mut sum_of_rolls = 0;
+    let mut sum = get_sum(&matrix);
+    // println!("{sum}");
+    sum_of_rolls += sum;
+
+    while sum > 0 {
+        matrix = delete_rolls(&matrix);
+        // print_matrix(&matrix);
+        sum = get_sum(&matrix);
+        // println!("{sum}");
+        sum_of_rolls += sum;
+    }
+
+    sum_of_rolls
 }
 
 fn run_it(get_sum: fn(&Vec<Vec<char>>) -> i32, vec: &Vec<Vec<char>>) -> (i32, f64) {
@@ -171,23 +189,27 @@ fn main() -> std::io::Result<()> {
     let file_2nd = std::fs::read_to_string(filename_2nd)?;
 
     println!("--- PART I -----------------------------------------------");
-    let vec = get_vec(&file_1st);
+    let matrix = get_matrix(&file_1st);
 
-    let (sum, took) = run_it(get_sum, &vec);
+    let (sum, took) = run_it(get_sum, &matrix);
     print_it(filename_1st, sum, took);
 
-    let vec = get_vec(&file_2nd);
+    let matrix = get_matrix(&file_2nd);
 
-    let (sum, took) = run_it(get_sum, &vec);
+    let (sum, took) = run_it(get_sum, &matrix);
     print_it(filename_2nd, sum, took);
 
-    //println!("----PART II ----------------------------------------------");
-    //let (sum, took) = run_it(get_sum, &file_1st, cells);
-    //print_it(filename_1st, sum, cells, took);
+    println!("----PART II ----------------------------------------------");
+    let matrix = get_matrix(&file_1st);
 
-    //let (sum, took) = run_it(get_sum, &file_2nd, cells);
-    //print_it(filename_2nd, sum, cells, took);
-    //println!("----------------------------------------------------------");
+    let (sum, took) = run_it(get_sum_of_rolls, &matrix);
+    print_it(filename_1st, sum, took);
+
+    let matrix = get_matrix(&file_2nd);
+
+    let (sum, took) = run_it(get_sum_of_rolls, &matrix);
+    print_it(filename_2nd, sum, took);
+    println!("----------------------------------------------------------");
 
     Ok(())
 }
